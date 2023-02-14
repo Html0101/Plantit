1d3f6ef88fd70fc60883b1b16f284bfc7d1c2563
Html0101 <102829836+Html0101@users.noreply.github.com>
2023-02-14 12:09:45 +0000

    Add files via upload

---
+++ YesHealth.js
@@ -0,0 +1,94 @@
+function back() 
+{ 
+    window.location = "Home.html";
+}
+
+function overav() 
+{ 
+    document.getElementById("aloe_vera_info").src = "Aloe Vera.png";
+}
+function leaveav() 
+{ 
+    document.getElementById("aloe_vera_info").src = "Aloe Vera.jpg";
+}
+
+function oversnp()
+{
+    document.getElementById("snake_plant_info").src = "Snake Plant.png";
+}
+function leavesnp() 
+{ 
+    document.getElementById("snake_plant_info").src = "Snake Plant.jpg";
+}
+
+function overbf()
+{
+    document.getElementById("boston_fern_info").src = "Boston Fern.png";
+}
+function leavebf() 
+{ 
+    document.getElementById("boston_fern_info").src = "Boston Fern.jpg";
+}
+
+function oversp()
+{
+    document.getElementById("spider_plant_info").src = "Spider Plant.png";
+}
+function leavesp() 
+{ 
+    document.getElementById("spider_plant_info").src = "Spider Plant.jpg";
+}
+
+function overa()
+{
+    document.getElementById("astragalus_info").src = "Astragalus.png";
+}
+function leavea() 
+{ 
+    document.getElementById("astragalus_info").src = "Astragalus.jpg";
+}
+
+function overca() 
+{ 
+    document.getElementById("cacoa_info").src = "Cacao.png";
+}
+function leaveca() 
+{ 
+    document.getElementById("cacoa_info").src = "Cacoa.jpg";
+}
+
+function overbb()
+{
+    document.getElementById("butchers_broom_info").src = "Butcher's Broom.png";
+}
+function leavebb() 
+{ 
+    document.getElementById("butchers_broom_info").src = "Butcher's Broom.jpg";
+}
+
+function overg()
+{
+    document.getElementById("garlic_info").src = "Garlic.png";
+}
+function leaveg() 
+{ 
+    document.getElementById("garlic_info").src = "Garlic.jpg";
+}
+
+function overo()
+{
+    document.getElementById("oregano_info").src = "Oregano.png";
+}
+function leaveo() 
+{ 
+    document.getElementById("oregano_info").src = "Oregano.jpg";
+}
+
+function overb()
+{
+    document.getElementById("bilberry_info").src = "Bilberry.png";
+}
+function leaveb() 
+{ 
+    document.getElementById("bilberry_info").src = "Bilberry.jpg";
+}
